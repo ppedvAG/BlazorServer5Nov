f1eeef0c51f1f84ea0ab4650de5d1efc1ded2fe9
Hannes Preishuber <hannesp@ppedv.de>
2020-11-19 16:58:16 +0100

    ToDo VM LocalStorage

--- BlazorServer5Nov/Pages/modul03/AufgabenVM.cs
+++ BlazorServer5Nov/Pages/modul03/AufgabenVM.cs
@@ -11,49 +11,54 @@ namespace BlazorServer5Nov.Pages.modul03
 {
     public class AufgabenVM
     {
-        IJSRuntime JSRuntime;
-        public AufgabenVM([FromServices] IJSRuntime _JSRuntime)
-        {
-            JSRuntime = _JSRuntime;
-        }
+        //IJSRuntime JSRuntime;
+        //public AufgabenVM([FromServices] IJSRuntime _JSRuntime)
+        //{
+        //    JSRuntime = _JSRuntime;
+        //}
         public List<Aufgabe> ListeAufgaben { get; set; }
         public void Speichern(Aufgabe a)
         {
 
             if (a.ID < 1) //NEU
             {
-                var id = ListeAufgaben.Max(x => x.ID) + 1;
-                a.ID = id;  
-                
+                if (ListeAufgaben.Count > 0)
+                {
+                    var id = ListeAufgaben.Max(x => x.ID);
+                    a.ID = id + 1;
+                }
+
+
                 ListeAufgaben.Add(a);
             }
             else //Update
             {
-                var _a = ListeAufgaben.Where(x=>x.ID==a.ID).FirstOrDefault();
+                var _a = ListeAufgaben.Where(x => x.ID == a.ID).FirstOrDefault();
                 _a.IsErledigt = a.IsErledigt;
                 _a.Text = a.Text;
             }
-       
-            var Wert = JsonSerializer.Serialize(ListeAufgaben);
 
-            JSRuntime.InvokeVoidAsync("localStorage.setItem", "Todos", Wert);
+            //var Wert = JsonSerializer.Serialize(ListeAufgaben);
+
+            //JSRuntime.InvokeVoidAsync("localStorage.setItem", "Todos", Wert);
 
 
         }
         public async void InitAsync()
         {
+            ListeAufgaben = new List<Aufgabe>();
 
-            try
-            {
-                var res = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "Todos");
-                ListeAufgaben = JsonSerializer.Deserialize<List<Aufgabe>>(res);
-                
-            }
-            catch (Exception)
-            {
+            //try
+            //{
+            //    var res = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "Todos");
+            //    ListeAufgaben = JsonSerializer.Deserialize<List<Aufgabe>>(res);
+
+            //}
+            //catch (Exception)
+            //{
 
 
-            }
+            //}
         }
 
     }
