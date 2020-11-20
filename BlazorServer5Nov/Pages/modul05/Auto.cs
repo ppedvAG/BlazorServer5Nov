c3accdda692607d8d168a2bdce20570b955df7cc
Hannes Preishuber <hannesp@ppedv.de>
2020-11-20 14:04:04 +0100

    Formular Fehlerprüfung visualissierung

---
+++ BlazorServer5Nov/Pages/modul05/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer5Nov.Pages.modul05
+{
+    public class Auto
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(5,ErrorMessage ="keine Automarke")]
+        public string Marke { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(1950,2030, ErrorMessage ="du umweltsau")]
+        public int Baujahr { get; set; }
+    }
+}
---
+++ BlazorServer5Nov/Pages/modul05/BoostrapClassProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer5Nov.Pages.modul05
+{
+    public class BoostrapClassProvider: FieldCssClassProvider
+    { 
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isValid ? "is-valid" : "is-invalid";
+        }
+    }
+}
